4cee4da788e5c3ba1bd2bc3eed35a3ae2efdfeb6
sb20mad <sb20mad@gmail.com>
2023-02-01 22:18:49 +0300

    final commit

--- program/obj/Debug/net6.0/program.AssemblyInfo.cs
+++ program/obj/Debug/net6.0/program.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Final_test")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Final_test")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Final_test")]
+[assembly: System.Reflection.AssemblyProductAttribute("program")]
+[assembly: System.Reflection.AssemblyTitleAttribute("program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
